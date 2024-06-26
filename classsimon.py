314cada40d08756cdbea7685a59273afe8ba3231
lucasbuloz <lucasbuloz@gmail.com>
2024-06-26 09:51:24 -0300

    finalizado

--- classsimon.py
+++ classsimon.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import random
-from tkinter import messagebox, Menu
+from tkinter import messagebox, Menu, ttk
 from gestor import GestorJugadores
 from classplayer import Jugador
 import datetime
@@ -11,141 +11,142 @@ class SimonDice:
         self.__ventana.title("Py-SimonGame")
         self.__ventana.geometry("300x350")  
 
-        self.color = ["#ff0000", "#00ff00", "#0000ff", "#ffff00"]
-        self.secuencia = []
-        self.secuencia_user = []
-        self.secuencia_user_active = False
-        self.gestor_jugadores = GestorJugadores("pysimonpuntajes.json")
+        self.__color = ["#ff0000", "#00ff00", "#0000ff", "#ffff00"]
+        self.__secuencia = []
+        self.__secuencia_user = []
+        self.__secuencia_user_active = False
+        self.__gestor_jugadores = GestorJugadores("pysimonpuntajes.json")
         
-        self.nombre_usuario = ""
-        self.puntaje = 0
+        self.__nombre_usuario = ""
+        self.__puntaje = 0
 
-        self.label_info = tk.Label(self.__ventana, text="Nombre: -- | Puntaje: 0", font=("Arial", 12))
-        self.label_info.pack(pady=10)
+        self.__label_info = tk.Label(self.__ventana, text="Nombre: -- | Puntaje: 0", font=("Arial", 12))
+        self.__label_info.pack(pady=10)
 
-        self.frame_botones = tk.Frame(self.__ventana)
-        self.frame_botones.pack(expand=True, fill="both")
+        self.__frame_botones = tk.Frame(self.__ventana)
+        self.__frame_botones.pack(expand=True, fill="both")
         
-        self.botonverde = self.create_button(self.color[0])
-        self.botonrojo = self.create_button(self.color[1])
-        self.botonazul = self.create_button(self.color[2])
-        self.botonamarillo = self.create_button(self.color[3])
+        self.__botonverde = self.create_button(self.__color[0])
+        self.__botonrojo = self.create_button(self.__color[1])
+        self.__botonazul = self.create_button(self.__color[2])
+        self.__botonamarillo = self.create_button(self.__color[3])
         
-        self.botonverde.grid(row=0, column=0, sticky="nsew")
-        self.botonrojo.grid(row=0, column=1, sticky="nsew")
-        self.botonazul.grid(row=1, column=0, sticky="nsew")
-        self.botonamarillo.grid(row=1, column=1, sticky="nsew")
+        self.__botonverde.grid(row=0, column=0, sticky="nsew")
+        self.__botonrojo.grid(row=0, column=1, sticky="nsew")
+        self.__botonazul.grid(row=1, column=0, sticky="nsew")
+        self.__botonamarillo.grid(row=1, column=1, sticky="nsew")
         
-        self.frame_botones.grid_rowconfigure(0, weight=1)
-        self.frame_botones.grid_rowconfigure(1, weight=1)
-        self.frame_botones.grid_columnconfigure(0, weight=1)
-        self.frame_botones.grid_columnconfigure(1, weight=1)
+        self.__frame_botones.grid_rowconfigure(0, weight=1)
+        self.__frame_botones.grid_rowconfigure(1, weight=1)
+        self.__frame_botones.grid_columnconfigure(0, weight=1)
+        self.__frame_botones.grid_columnconfigure(1, weight=1)
         
         self.mostrar_dialogo_usuario()
         self.crear_menu()
 
     def create_button(self, color):
-        canvas = tk.Canvas(self.frame_botones, bg=color, width=100, height=100, relief="raised")
+        canvas = tk.Canvas(self.__frame_botones, bg=color, width=100, height=100, relief="raised")
         canvas.bind("<Button-1>", lambda event, c=color: self.on_button_click(event, canvas, c))
         return canvas
 
     def iniciar_juego(self):
-        self.secuencia = []
-        self.secuencia_user = []
-        self.secuencia_user_active = False
-        self.puntaje = 0
+        self.__secuencia = []
+        self.__secuencia_user = []
+        self.__secuencia_user_active = False
+        self.__puntaje = 0
         self.actualizar_info()
         self.agregar_color_juego()
 
     def agregar_color_juego(self):
-        self.secuencia.append(random.choice(self.color))
+        self.__secuencia.append(random.choice(self.__color))
         self.mostrar_secuencia()
 
     def mostrar_secuencia(self):
         delay = 1000  
-        for i, color in enumerate(self.secuencia):
+        for i, color in enumerate(self.__secuencia):
             self.__ventana.after(delay * i, lambda color=color: self.cambiar_color_botones(color))
             self.__ventana.after(delay * i + delay // 2, self.restaurar_color_botones)
-        self.__ventana.after(delay * len(self.secuencia), self.activar_botones)
+        self.__ventana.after(delay * len(self.__secuencia), self.activar_botones)
 
     def cambiar_color_botones(self, color):
-        if color == self.color[0]:
-            self.botonverde.config(bg="#ffffff")
-        elif color == self.color[1]:
-            self.botonrojo.config(bg="#ffffff")
-        elif color == self.color[2]:
-            self.botonazul.config(bg="#ffffff")
-        elif color == self.color[3]:
-            self.botonamarillo.config(bg="#ffffff")
+        if color == self.__color[0]:
+            self.__botonverde.config(bg="#ffffff")
+        elif color == self.__color[1]:
+            self.__botonrojo.config(bg="#ffffff")
+        elif color == self.__color[2]:
+            self.__botonazul.config(bg="#ffffff")
+        elif color == self.__color[3]:
+            self.__botonamarillo.config(bg="#ffffff")
 
     def restaurar_color_botones(self):
-        self.botonverde.config(bg=self.color[0])
-        self.botonrojo.config(bg=self.color[1])
-        self.botonazul.config(bg=self.color[2])
-        self.botonamarillo.config(bg=self.color[3])
+        self.__botonverde.config(bg=self.__color[0])
+        self.__botonrojo.config(bg=self.__color[1])
+        self.__botonazul.config(bg=self.__color[2])
+        self.__botonamarillo.config(bg=self.__color[3])
 
     def on_button_click(self, event, canvas, color):
-        if not self.secuencia_user_active:
+        if not self.__secuencia_user_active:
             return
         canvas.config(bg="#ffffff")
         self.__ventana.after(500, lambda: canvas.config(bg=color))
-        self.secuencia_user.append(color)
-        if self.secuencia[:len(self.secuencia_user)] == self.secuencia_user:
-            if len(self.secuencia) == len(self.secuencia_user):
-                self.puntaje += 1
+        self.__secuencia_user.append(color)
+        if self.__secuencia[:len(self.__secuencia_user)] == self.__secuencia_user:
+            if len(self.__secuencia) == len(self.__secuencia_user):
+                self.__puntaje += 1
                 self.actualizar_info()
-                self.secuencia_user = []
+                self.__secuencia_user = []
+                self.__secuencia_user_active = []
                 self.desactivar_botones()
                 self.__ventana.after(1000, self.agregar_color_juego)
         else:
-            messagebox.showinfo("GAME OVER", f"GAME OVER. Tu puntuación final es: {self.puntaje}")
+            messagebox.showinfo("GAME OVER", f"GAME OVER. Tu puntuación final es: {self.__puntaje}")
             self.guardar_puntaje_final()
             self.reset_juego()
 
     def guardar_puntaje_final(self):
         fecha_actual = datetime.datetime.now().strftime("%Y-%m-%d")
         hora_actual = datetime.datetime.now().strftime("%H:%M:%S")
-        nuevo_jugador = Jugador(self.nombre_usuario, fecha_actual, hora_actual, self.puntaje)
-        self.gestor_jugadores.guardar_jugador(nuevo_jugador)
+        nuevo_jugador = Jugador(self.__nombre_usuario, fecha_actual, hora_actual, self.__puntaje)
+        self.__gestor_jugadores.guardar_jugador(nuevo_jugador)
 
     def desactivar_botones(self):
-        self.botonverde.unbind("<Button-1>")
-        self.botonrojo.unbind("<Button-1>")
-        self.botonazul.unbind("<Button-1>")
-        self.botonamarillo.unbind("<Button-1>")
-        self.secuencia_user_active = False
+        self.__botonverde.unbind("<Button-1>")
+        self.__botonrojo.unbind("<Button-1>")
+        self.__botonazul.unbind("<Button-1>")
+        self.__botonamarillo.unbind("<Button-1>")
+        self.__secuencia_user_active = False
 
     def activar_botones(self):
-        self.botonverde.bind("<Button-1>", lambda event: self.on_button_click(event, self.botonverde, self.color[0]))
-        self.botonrojo.bind("<Button-1>", lambda event: self.on_button_click(event, self.botonrojo, self.color[1]))
-        self.botonazul.bind("<Button-1>", lambda event: self.on_button_click(event, self.botonazul, self.color[2]))
-        self.botonamarillo.bind("<Button-1>", lambda event: self.on_button_click(event, self.botonamarillo, self.color[3]))
-        self.secuencia_user_active = True
+        self.__botonverde.bind("<Button-1>", lambda event: self.on_button_click(event, self.__botonverde, self.__color[0]))
+        self.__botonrojo.bind("<Button-1>", lambda event: self.on_button_click(event, self.__botonrojo, self.__color[1]))
+        self.__botonazul.bind("<Button-1>", lambda event: self.on_button_click(event, self.__botonazul, self.__color[2]))
+        self.__botonamarillo.bind("<Button-1>", lambda event: self.on_button_click(event, self.__botonamarillo, self.__color[3]))
+        self.__secuencia_user_active = True
 
     def mostrar_dialogo_usuario(self):
         self.__dialogo = tk.Toplevel(self.__ventana)
         self.__dialogo.geometry('300x200')
         self.__dialogo.resizable(0, 0)
         tk.Label(self.__dialogo, text="Nombre de Usuario:").pack()
-        self.nombre_entrada = tk.Entry(self.__dialogo)
-        self.nombre_entrada.pack()
-        self.boton_confirmar = tk.Button(self.__dialogo, text='Confirmar', command=self.guardar_nombre_usuario)
-        self.boton_confirmar.pack()
+        self.__nombre_entrada = tk.Entry(self.__dialogo)
+        self.__nombre_entrada.pack()
+        self.__boton_confirmar = tk.Button(self.__dialogo, text='Confirmar', command=self.guardar_nombre_usuario)
+        self.__boton_confirmar.pack()
 
     def guardar_nombre_usuario(self):
-        self.nombre_usuario = self.nombre_entrada.get()
+        self.__nombre_usuario = self.__nombre_entrada.get()
         self.__dialogo.destroy()
         self.iniciar_juego()
 
     def reset_juego(self):
-        self.secuencia = []
-        self.secuencia_user = []
-        self.secuencia_user_active = False
-        self.puntaje = 0
+        self.__secuencia = []
+        self.__secuencia_user = []
+        self.__secuencia_user_active = False
+        self.__puntaje = 0
         self.iniciar_juego()
 
     def actualizar_info(self):
-        self.label_info.config(text=f"Nombre: {self.nombre_usuario} | Puntaje: {self.puntaje}")
+        self.__label_info.config(text=f"Nombre: {self.__nombre_usuario} | Puntaje: {self.__puntaje}")
 
     def crear_menu(self):
         menubar = Menu(self.__ventana)
@@ -157,7 +158,27 @@ class SimonDice:
         opciones_menu.add_command(label="Salir", command=self.__ventana.quit)
 
     def ver_puntajes(self):
-        self.gestor_jugadores.ver_puntajes()
+        puntajes_ventana = tk.Toplevel(self.__ventana)
+        puntajes_ventana.title("Puntajes")
+        puntajes_ventana.geometry("400x300")
+
+        tree = ttk.Treeview(puntajes_ventana, columns=('Jugador', 'Fecha', 'Hora', 'Puntaje'), show='headings')
+        tree.heading('Jugador', text='Jugador')
+        tree.heading('Fecha', text='Fecha')
+        tree.heading('Hora', text='Hora')
+        tree.heading('Puntaje', text='Puntaje')
+        
+        tree.column('Jugador', width=50)
+        tree.column('Fecha', width=50)
+        tree.column('Hora', width=50)
+        tree.column('Puntaje', width=20)
+
+        tree.pack(expand=True, fill='both')
+
+        self.__gestor_jugadores.ver_puntajes(tree)
+
+        close_button = tk.Button(puntajes_ventana, text='Cerrar', command=puntajes_ventana.destroy)
+        close_button.pack(pady=10)
 
 if __name__ == '__main__':
     ventana = tk.Tk()
--- gestor.py
+++ gestor.py
@@ -1,29 +1,28 @@
-from classplayer import Jugador
 import json
-from tkinter import messagebox
+from classplayer import Jugador
 
 class GestorJugadores:
     def __init__(self, archivo):
-        self.archivo = archivo  # ruta del archivo json
-        self.jugadores = self.cargar_jugadores()
+        self.__archivo = archivo  # ruta del archivo json
+        self.__jugadores = self.cargar_jugadores()
 
     def cargar_jugadores(self):
         try:
-            with open(self.archivo, 'r') as file:
+            with open(self.__archivo, 'r') as file:
                 data = json.load(file)
                 return [Jugador.from_dict(jugador) for jugador in data]
         except FileNotFoundError:
             return []
 
     def guardar_jugador(self, jugador):
-        self.jugadores.append(jugador)
+        self.__jugadores.append(jugador)
         self.guardar_jugadores()
 
     def guardar_jugadores(self):
-        with open(self.archivo, 'w') as file:
-            json.dump([jugador.to_dict() for jugador in self.jugadores], file, indent=4)
+        with open(self.__archivo, 'w') as file:
+            json.dump([jugador.to_dict() for jugador in self.__jugadores], file, indent=4)
 
-    def ver_puntajes(self):
-        self.jugadores.sort(reverse=True)
-        puntajes = "\n".join([f"Nombre: {jugador.get_nombre()}, Puntaje: {jugador.get_puntaje()}, Fecha: {jugador.get_fecha()}, Hora: {jugador.get_hora()}" for jugador in self.jugadores])
-        messagebox.showinfo("Puntajes", puntajes)
+    def ver_puntajes(self, tree):
+        self.__jugadores.sort(key=lambda x: x.get_puntaje(), reverse=True)
+        for jugador in self.__jugadores:
+            tree.insert('', 'end', values=(jugador.get_nombre(), jugador.get_fecha(), jugador.get_hora(), jugador.get_puntaje()))
